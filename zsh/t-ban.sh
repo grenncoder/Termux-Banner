94439093d1674af8844bc77c12741927fca31ac8
grenncoder <38747138+grenncoder@users.noreply.github.com>
2018-04-26 06:36:44 +0000

    Create t-ban.sh

---
+++ zsh/t-ban.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+2
+#
+3
+#
+4
+# Termux Banner
+5
+# Banner by Faisal (Gr33n.Cod3r)
+6
+# _____Do Not Copy Or Modify_____
+7
+#
+8
+#
+9
+clear
+10
+figlet -f big Termux Banner | lolcat
+11
+cyan='\e[0;36m'
+12
+lightgreen='\e[1;32m'
+13
+red='\e[1;31m'
+14
+yellow='\e[1;33m'
+15
+echo -e $lightgreen "\e[1m                Security Help For Ethical Hackers... "
+16
+echo " "
+17
+echo -e $yellow "\e[1m                         -Faisal "
+18
+echo " "
+19
+echo " "
+20
+echo " "
+21
+echo -e "\e[1m\e[33m\nWhat is Your \e[31mBanner \e[33mName\e[32m :\n\n"
+22
+read varbanner
+23
+echo
+24
+echo -e "\e[1m\e[33m\nWhat is Your Cowsay Name\e[32m :\n\n "
+25
+read varcowsay
+26
+echo  "cowsay -f eyes "$varcowsay" | lolcat" > C0w54y.txt
+27
+echo "toilet -f big '    $varbanner' -F gay | lolcat" > 84nn3r.txt
+28
+echo
+29
+echo "clear" > cl34r.txt
+30
+cat "cl34r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+31
+cat "C0w54y.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+32
+ls
+33
+cat "84nn3r.txt" >> /data/data/com.termux/files/usr/etc/zshrc
+34
